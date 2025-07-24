514cf78ddf5099250bca42ed748e66e9164b5140
rohitwebdeveloper <you@example.com>
2025-07-24 11:00:31 +0530

    Add helmet, hpp, compression and express ratelimit middlewares

--- app.js
+++ app.js
@@ -1,37 +1,58 @@
- const express = require('express')
- const cookieParser = require('cookie-parser')
- const cors = require('cors')
- require('dotenv').config()
- const errorHandler = require('./middlewares/errorHandler')
- const authRoutes = require('./routes/authRoutes')
- const photoRoutes = require('./routes/photoRoutes')
- const albumRoutes = require('./routes/albumRoutes')
- const documentRoutes = require('./routes/documentRoutes')
- const userRoutes = require('./routes/userRoutes')
- 
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const helmet = require('helmet')
+const compression = require('compression')
+const hpp = require('hpp')
+const rateLimit = require('express-rate-limit')
+require('dotenv').config()
+const errorHandler = require('./middlewares/errorHandler')
+const authRoutes = require('./routes/authRoutes')
+const photoRoutes = require('./routes/photoRoutes')
+const albumRoutes = require('./routes/albumRoutes')
+const documentRoutes = require('./routes/documentRoutes')
+const userRoutes = require('./routes/userRoutes')
+
 
 // Initialize express 
- const app = express()
+const app = express()
+
+
+// Security middlewares
+app.use(helmet())
+app.use(hpp())
+
+
+// Rate Limiter to limit the request by user
+const limiter = rateLimit({
+    windowMs: 600000,
+    max: 100,
+    message: "Too many requests from your side, please try again later",
+})
+app.use(limiter)
+
+// Compress data sent on request
+app.use(compression())
 
 
 //  Middlewares
- app.use(express.json())
- app.use(express.urlencoded({extended:true}))
- app.use(cors({origin:process.env.CLIENT_URL, credentials:true}))
- app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
+app.use(cookieParser())
 
 
 //  Define Routes
- app.use('/api/auth', authRoutes)
- app.use('/api/photo', photoRoutes)
- app.use('/api/album', albumRoutes)
- app.use('/api/document', documentRoutes)
- app.use('/api/user', userRoutes)
- 
+app.use('/api/auth', authRoutes)
+app.use('/api/photo', photoRoutes)
+app.use('/api/album', albumRoutes)
+app.use('/api/document', documentRoutes)
+app.use('/api/user', userRoutes)
+
 
 
 //  Global error handler
- app.use(errorHandler)
+app.use(errorHandler)
 
 
 module.exports = app
